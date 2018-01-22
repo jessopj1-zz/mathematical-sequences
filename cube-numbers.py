f2eaaae1027f4309751643ef1d3ce45cec9f9b7b
John Jessop <jessopjohn1@gmail.com>
2018-01-22 17:23:30 +0000

    New thing
    Cube numbers

---
+++ cube-numbers.py
@@ -0,0 +1,15 @@
+import os
+order = 1
+print("This is John's cube number generator.")
+print("The first is ", value)
+value = int(value) + int(value2)
+iterations = input("How many cube numbers would you like to generate? I suggest at least 10: ")
+print("You chose to get the fisrt ", iterations, " Fibonacci numbers" )
+
+while (order < int(iterations)):
+    order = int(order) + 1
+    value = order**3
+    print("Value = ", value)
+
+print("That is the first ",iterations ," cube numbers")
+os.system("pause")
