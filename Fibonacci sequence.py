aaac9df71af12368d69ec4f98daf8334bdf47521
John Jessop <jessopjohn1@gmail.com>
2018-01-22 17:15:28 +0000

    Few fixes
    It works now

--- Fibonacci sequence.py
+++ Fibonacci sequence.py
@@ -6,15 +6,15 @@ print("This is John's Fibonacci number generator.")
 print("Number in sequence = ", order)
 print("Value = ", value)
 value = int(value) + int(value2)
-iteratons = input("How many Fibonacci numbers would you like to generate? I suggest at least 100")
-print("You chose to get the fisrt ", iteratons, " Fibonacci numbers" )
+iterations = input("How many Fibonacci numbers would you like to generate? I suggest at least 100: ")
+print("You chose to get the fisrt ", iterations, " Fibonacci numbers" )
 
-while (order < iteratons):
+while (order < int(iterations)):
     order = int(order) + 1
     prevvalue = value
     value = value + value2
     print("Value = ", value)
     value2 = prevvalue
 
-print("That is the first 100 numbers of the Fibonacci sequence")
+print("That is the first ",iterations ," numbers of the Fibonacci sequence")
 os.system("pause")
